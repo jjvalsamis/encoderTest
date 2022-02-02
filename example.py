f50e51f8cd8057aea12d4e8bb3e32495821a7af1
jjvalsamis <62852149+jjvalsamis@users.noreply.github.com>
2022-02-01 23:16:16 -0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+from time import sleep_ms
+from AS5600L import AS5600L
+
+
+
+myPos = AS5600L(hyst=1)
+
+print(myPos.getStatus())
+
+while True:
+    print('Degrees:', myPos.getAngleDegrees())
+    sleep_ms(333)
+
